bd8f1f8c468aaa3fd7b1796d576ff78ae07b9126
tommaso.ventafridda <tommaso.ventafridda@gmail.com>
2018-12-28 00:22:34 +0100

    Prebuild

---
+++ nodb.php
@@ -0,0 +1,11 @@
+<?php
+/* Initial Requires */
+require_once("modules/configuration.php");
+require_once("modules/storage.php");
+require_once("modules/math.php");
+
+/* Functions Requires */
+require_once("modules/language.php");
+
+/* */
+?>
