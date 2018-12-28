4358de3844c190664770b6b443c12b852c22775d
tommaso.ventafridda <tommaso.ventafridda@gmail.com>
2018-12-28 16:32:15 +0100

    Initial class for storage management

---
+++ modules/storage.php
@@ -0,0 +1,33 @@
+<?php
+class storage{
+    public function read_small_file($path,$debug_checktime=false){
+        if ($debug_checktime){$init_time = microtime(true);}
+        $content = file_get_contents($path);
+        if ($debug_checktime){
+            return (microtime(true)-$init_time)*10000;
+        }else{
+            return $content;
+        }
+    }
+    
+    public function read_large_file($path,$debug_checktime=false){
+        $content = implode('',file($path));
+        return $content;
+    }
+    
+    public function write_small_file($path,$content,$only_add=false){
+        $content = ($only_add)?read_small_file($path).$content:$content;
+        $file = fopen($path, "w");
+        fwrite($file, $content);
+        fclose($file);
+    }
+    
+    public function write_large_file($path,$content,$only_add=false){
+        $content = ($only_add)?read_small_file($path).$content:$content;
+        $file = fopen($path, "w");
+        fwrite($file, $content);
+        fclose($file);
+    }
+}
+
+?>
