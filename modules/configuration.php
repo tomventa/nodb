df73224b93a4ccfd7b002f50f9baac910a03bf5b
tommaso.ventafridda <tommaso.ventafridda@gmail.com>
2019-01-04 22:06:46 +0100

    Added performance variables
    Signed-off-by: tommaso.ventafridda <tommaso.ventafridda@gmail.com>

--- modules/configuration.php
+++ modules/configuration.php
@@ -2,5 +2,10 @@
 /* Set memory limit to 512MB */
 ini_set("memory_limit","512M");
 
+/* NoDB Performance Settings */
+$nodb_conf_max_databases = 256;
+$nodb_conf_max_tables = 256;
+$nodb_conf_max_columns = 512;
+
 
 ?>
